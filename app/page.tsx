45680973dbcdbf6b5545d0d7c868a4c69b5927f8
Ajonastech <Agujonas13@gmail.com>
2026-08-14 22:43:58 +0100

    chore: yo! the first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Deal Room</h1>
+      <p>Lets get this done with</p>
+    </div>
+  );
+}
